1d18cf7211734498eb8e453c864c90282a84f2e0
WNxClivien <steeleta@dukes.jmu.edu>
2022-03-23 18:49:34 -0400

    Added CreateAccount Web Form

---
+++ Sprint1/CreateAccount.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CreateAccount.aspx.cs" Inherits="Sprint1.CreateAccount" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3>Create Account:</h3>
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">Firstname:</td>
+                <td>
+                    <asp:TextBox ID="txtFirstName" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">LastName:</td>
+                <td class="auto-style4">
+                    <asp:TextBox ID="txtLastName" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Email:</td>
+                <td>
+                    <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Username:</td>
+                <td>
+                    <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Password:</td>
+                <td>
+                    <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">Student or Member:</td>
+                <td>
+                    <asp:DropDownList ID="ddlPersonType" runat="server">
+                        <asp:ListItem>Student</asp:ListItem>
+                        <asp:ListItem>Member</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+            </tr>
+        </table>
+                    <asp:Button ID="btnCreateUser" runat="server" Text="Create Account" OnClick="btnCreateUser_Click"/>
+        <br />
+        <asp:LinkButton ID="lnkAnother" runat="server" OnClick="lnkAnother_Click" Visible="False">Create Another Account</asp:LinkButton>
+        <br />
+        <asp:Label ID="lblStatus" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Sprint1/CreateAccount.aspx.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace Sprint1
+{
+    public partial class CreateAccount : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCreateUser_Click(object sender, EventArgs e)
+        {
+            if (isDuplicate() == false)
+            {
+                if (txtFirstName.Text != "" && txtLastName.Text != "" && txtEmail.Text != "" && txtPassword.Text != "" && txtUsername.Text != "") // all fields must be filled out
+                {
+                    // COMMIT VALUES
+                    try
+                    {
+                        System.Data.SqlClient.SqlConnection sc = new SqlConnection(WebConfigurationManager.ConnectionStrings["AUTH"].ConnectionString.ToString());
+                        lblStatus.Text = "Database Connection Successful";
+
+                        sc.Open();
+
+                        System.Data.SqlClient.SqlCommand createUser = new System.Data.SqlClient.SqlCommand();
+                        createUser.Connection = sc;
+
+                        // INSERT USER RECORD
+                        createUser.CommandText = "INSERT INTO Person (FirstName, LastName, Email, PersonType, Username) VALUES (@FName, @LName, @Email, " +
+                            "@PType, @Username)";
+                        createUser.Parameters.Add(new SqlParameter("@FName", txtFirstName.Text));
+                        createUser.Parameters.Add(new SqlParameter("@LName", txtLastName.Text));
+                        createUser.Parameters.Add(new SqlParameter("@Email", txtEmail.Text));
+                        createUser.Parameters.Add(new SqlParameter("@PType", ddlPersonType.Text));
+                        createUser.Parameters.Add(new SqlParameter("@Username", txtUsername.Text));
+                        createUser.ExecuteNonQuery();
+
+
+                        System.Data.SqlClient.SqlCommand setPass = new System.Data.SqlClient.SqlCommand();
+                        setPass.Connection = sc;
+                        // INSERT PASSWORD RECORD AND CONNECT TO USER
+                        setPass.CommandText = "INSERT INTO Pass (UserID, Username, PasswordHash, Activation) VALUES ((select max(userid) from person), " +
+                            "@Username, @Password, 'Inactive')";
+                        setPass.Parameters.Add(new SqlParameter("@Username", txtUsername.Text));
+                        setPass.Parameters.Add(new SqlParameter("@Password", PasswordHash.HashPassword(txtPassword.Text))); // hash entered password
+                        setPass.ExecuteNonQuery();
+
+                        sc.Close();
+
+                        // INSERT INTO STUDENT/MEMBER TABLE
+                        System.Data.SqlClient.SqlConnection sc2 = new SqlConnection(WebConfigurationManager.ConnectionStrings["SDB"].ConnectionString.ToString());
+                        sc2.Open();
+
+                        if (ddlPersonType.Text == "Student")
+                        {
+                            System.Data.SqlClient.SqlCommand createStudent = new System.Data.SqlClient.SqlCommand();
+                            createStudent.Connection = sc2;
+
+                            // INSERT STUDENT RECORD
+                            createStudent.CommandText = "INSERT INTO Student (FirstName, LastName, EmailAddress, PhoneNumber, GradYear, Major, " +
+                                "Grade, EmploymentStatus, StudentUserName) VALUES (@FName, @LName, @EmailAddress, 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', @StudentUserName)";
+                            createStudent.Parameters.Add(new SqlParameter("@FName", txtFirstName.Text));
+                            createStudent.Parameters.Add(new SqlParameter("@LName", txtLastName.Text));
+                            createStudent.Parameters.Add(new SqlParameter("@EmailAddress", txtEmail.Text));
+                            createStudent.Parameters.Add(new SqlParameter("@StudentUserName", txtUsername.Text));
+                            createStudent.ExecuteNonQuery();
+                        }
+                        else
+                        {
+                            System.Data.SqlClient.SqlCommand createMember = new System.Data.SqlClient.SqlCommand();
+                            createMember.Connection = sc2;
+
+                            // INSERT MEMBER RECORD
+                            createMember.CommandText = "INSERT INTO Member (FirstName, LastName, EmailAddress, PhoneNumber, Title, MemberUserName, AccountType) " +
+                                "VALUES (@FName, @LName, @EmailAddress, 'N/A', 'N/A', @MemberUserName, 'Member')";
+                            createMember.Parameters.Add(new SqlParameter("@FName", txtFirstName.Text));
+                            createMember.Parameters.Add(new SqlParameter("@LName", txtLastName.Text));
+                            createMember.Parameters.Add(new SqlParameter("@EmailAddress", txtEmail.Text));
+                            createMember.Parameters.Add(new SqlParameter("@MemberUserName", txtUsername.Text));
+                            createMember.ExecuteNonQuery();
+
+                        }
+                        // CLOSE DB CONNECTION
+                        sc2.Close();
+
+                        // GREY OUT TEXT BOXES
+                        lblStatus.Text = "Account Request Submitted! You will be emailed soon with your verification.";
+                        txtFirstName.Enabled = false;
+                        txtLastName.Enabled = false;
+                        txtEmail.Enabled = false;
+                        ddlPersonType.Enabled = false;
+                        txtUsername.Enabled = false;
+                        txtPassword.Enabled = false;
+                        btnCreateUser.Enabled = false;
+                        lnkAnother.Visible = true;
+                    }
+                    catch
+                    {
+                        // ERROR MESSAGE
+                        lblStatus.Text = "Database Error - Account Request Not Submitted.";
+                    }
+                }
+                else
+                {
+                    // ERROR IF ALL FIELDS HAVE NOT BEEN ENTERED
+                    lblStatus.Text = "All Fields Must Be Filled!";
+                }
+            }
+            else
+            {
+                // ERROR IF PERSON HAS AN ACCOUNT WITH THE SAME USERNAME
+                lblStatus.Text = "There is already an account with this Username!";
+            }
+        }
+
+        // CREATE ANOTHER ACCOUNT REQUEST
+        protected void lnkAnother_Click(object sender, EventArgs e)
+        {
+            // MAKE TEXT BOXES VISIBLE 
+            txtFirstName.Enabled = true;
+            txtLastName.Enabled = true;
+            txtEmail.Enabled = true;
+            ddlPersonType.Enabled = true;
+            txtUsername.Enabled = true;
+            txtPassword.Enabled = true;
+            btnCreateUser.Enabled = true;
+            lnkAnother.Visible = false;
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtEmail.Text = "";
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+            lblStatus.Text = "";
+        }
+
+        // Method to check for Duplicate entries
+        protected Boolean isDuplicate()
+        {
+            try
+            {
+                // Get username from the prompt
+                string username = this.txtUsername.Text;
+
+                // create Query
+                String sqlQuery = "SELECT UserID, FirstName, LastName, Email, Username FROM Person WHERE Username='" + username + "'";
+
+                // Define Connection to DB
+                SqlConnection sqlConnect = new SqlConnection
+                    (WebConfigurationManager.ConnectionStrings["AUTH"].ConnectionString);
+
+                // Create SQL Command (Sends query to the DB
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnect;
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.CommandText = sqlQuery;
+
+                // Issue the query and retrieve the results
+                sqlConnect.Open();
+                SqlDataReader queryResults = sqlCommand.ExecuteReader();
+
+                if (queryResults.Read())
+                {
+                    //Close DB Connection
+                    sqlConnect.Close();
+                    queryResults.Close();
+
+                    //return true
+                    return true;
+                }
+                else
+                {
+                    //Close DB Connection
+                    sqlConnect.Close();
+                    queryResults.Close();
+
+                    //return false
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}

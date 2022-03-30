295f46186508c31638f08b469be2d5a88fb82a2c
WNxClivien <steeleta@dukes.jmu.edu>
2022-03-30 15:47:58 -0400

    added admin authorizations of users

--- Sprint1/AccountRequests.aspx.cs
+++ Sprint1/AccountRequests.aspx.cs
@@ -5,6 +5,10 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
 namespace Sprint1
 {
     public partial class AccountRequests : System.Web.UI.Page
@@ -12,6 +16,33 @@ namespace Sprint1
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnAuthorizeAccount_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String sqlQuery = "UPDATE Pass SET Activation = 'Active' WHERE UserID =" + dvUnauthorizedUsers.SelectedValue;
+
+                System.Data.SqlClient.SqlConnection sqlConnect = new SqlConnection(WebConfigurationManager.ConnectionStrings["AUTH"].ConnectionString.ToString());
+
+                // create and structure query command for insert
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnect;
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.CommandText = sqlQuery;
+
+                // execute query and retrieve results
+                sqlConnect.Open();
+                sqlCommand.ExecuteScalar();
+                sqlConnect.Close();
+
+                Response.Redirect("AccountRequests.aspx");
+            }
+            catch (Exception)
+            {
+
+            }
         }
     }
 }

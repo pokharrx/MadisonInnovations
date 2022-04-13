171db50d634fa19d19e0e0a1820176a637310334
WNxClivien <steeleta@dukes.jmu.edu>
2022-04-13 19:27:43 -0400

    added Master Admin privilege to make members an admin, or an admin a member.

---
+++ Sprint1/AdminPrivileges.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/adminMaster.Master" AutoEventWireup="true" CodeBehind="AdminPrivileges.aspx.cs" Inherits="Sprint1.AdminPrivileges" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Label ID="lblWelcome" runat="server" Text="Select a member to upgrade to an admin:"></asp:Label>
+    <asp:Label ID="lblStatus" runat="server" Text="You are not an admin, access denied." Visible="false"></asp:Label>
+    <br />
+    <asp:DropDownList ID="ddlMembersUpgrade" runat="server"></asp:DropDownList>
+    <br />
+    <asp:Button ID="btnUpgrade" runat="server" Text="Promote" OnClick="btnUpgrade_Click"/>
+    <br />
+    <asp:Label ID="lblDowngrade" runat="server" Text="Select an admin to downgrade to a member:"></asp:Label>
+    <br />
+    <asp:DropDownList ID="ddlAdmins" runat="server"></asp:DropDownList>
+    <br />
+    <asp:Button ID="btnDowngrade" runat="server" Text="Demote" OnClick="btnDowngrade_Click"/>
+
+
+</asp:Content>
---
+++ Sprint1/AdminPrivileges.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Drawing;
+
+namespace Sprint1
+{
+    public partial class AdminPrivileges : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Username"].ToString() != "admin")
+            {
+                lblWelcome.Visible = false;
+                lblStatus.Visible = true;
+                lblStatus.ForeColor = Color.Red;
+            }
+
+            try
+            {
+                // Define Connection to DB
+                SqlConnection sqlConnect = new SqlConnection
+                    (WebConfigurationManager.ConnectionStrings["SDB"].ConnectionString);
+
+                // Create Query
+                String sqlQuery = "SELECT FirstName + ' ' + LastName AS Name, MemberID FROM Member WHERE AccountType='Member'";
+                // Create SQL Command (Sends query to the DB
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnect;
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.CommandText = sqlQuery;
+
+                // Issue the query and retrieve the results
+                sqlConnect.Open();
+                SqlDataReader queryResults = sqlCommand.ExecuteReader();
+
+                while (queryResults.Read())
+                {
+                    ListItem item = new ListItem();
+                    item.Text = queryResults["Name"].ToString();
+                    item.Value = queryResults["MemberID"].ToString();
+                    ddlMembersUpgrade.Items.Add(item);
+                }
+
+                //Close DB Connection
+                sqlConnect.Close();
+                queryResults.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            try
+            {
+                // Define Connection to DB
+                SqlConnection sqlConnect = new SqlConnection
+                    (WebConfigurationManager.ConnectionStrings["SDB"].ConnectionString);
+
+                // Create Query
+                String sqlQuery = "SELECT FirstName + ' ' + LastName AS Name, MemberID FROM Member WHERE AccountType='Admin' AND MemberUserName != 'admin'";
+                // Create SQL Command (Sends query to the DB
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnect;
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.CommandText = sqlQuery;
+
+                // Issue the query and retrieve the results
+                sqlConnect.Open();
+                SqlDataReader queryResults = sqlCommand.ExecuteReader();
+
+                while (queryResults.Read())
+                {
+                    ListItem item = new ListItem();
+                    item.Text = queryResults["Name"].ToString();
+                    item.Value = queryResults["MemberID"].ToString();
+                    ddlAdmins.Items.Add(item);
+                }
+
+                //Close DB Connection
+                sqlConnect.Close();
+                queryResults.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        protected void btnUpgrade_Click(object sender, EventArgs e)
+        {
+            // Define Connection to DB
+            SqlConnection sqlConnect = new SqlConnection
+                (WebConfigurationManager.ConnectionStrings["SDB"].ConnectionString);
+
+            // Create Query
+            String sqlQuery = "UPDATE Member SET AccountType='Admin' WHERE MemberID=" + ddlMembersUpgrade.SelectedValue;
+            // Create SQL Command (Sends query to the DB
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = sqlConnect;
+            sqlCommand.CommandType = CommandType.Text;
+            sqlCommand.CommandText = sqlQuery;
+
+            // Issue the query and retrieve the results
+            sqlConnect.Open();
+            SqlDataReader queryResults = sqlCommand.ExecuteReader();
+
+            //Close DB Connection
+            sqlConnect.Close();
+            queryResults.Close();
+        }
+
+        protected void btnDowngrade_Click(object sender, EventArgs e)
+        {
+            // Define Connection to DB
+            SqlConnection sqlConnect = new SqlConnection
+                (WebConfigurationManager.ConnectionStrings["SDB"].ConnectionString);
+
+            // Create Query
+            String sqlQuery = "UPDATE Member SET AccountType='Member' WHERE MemberID=" + ddlMembersUpgrade.SelectedValue;
+            // Create SQL Command (Sends query to the DB
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = sqlConnect;
+            sqlCommand.CommandType = CommandType.Text;
+            sqlCommand.CommandText = sqlQuery;
+
+            // Issue the query and retrieve the results
+            sqlConnect.Open();
+            SqlDataReader queryResults = sqlCommand.ExecuteReader();
+
+            //Close DB Connection
+            sqlConnect.Close();
+            queryResults.Close();
+        }
+    }
+}
